4b1bcf1dea293f1384c61bf22f9d63e32179526e
0525stone <0525stone@gmail.com>
2022-05-29 19:08:23 +0900

    json read 까지 done

--- tools/data_refine.py
+++ tools/data_refine.py
@@ -7,8 +7,8 @@ def read_file(filename=''):
         with open(filename, 'r') as f:
             data = json.load(f)
             data = data['data'] # aihub 기계독해의 경우
-            print(f'type\t{type(data)}\nkeys\t{data.keys()}')
-            
+            print(f'type\t{type(data)}\nkeys\t{data[0]}')
+            print(json.dumps(data[0], indent='\t', ensure_ascii=False))
 
 
 
@@ -17,6 +17,7 @@ def main():
     print(f'{"*"*50}    start    {"*"*50}')
     filename = './data/aihub_mrc/ex1.json'
     read_file(filename)
+    
 
     # 최종 파일은 tsv 형식
     # # id, context, question, answer_text, answer_start
