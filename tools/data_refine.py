cbae0da64ea6667384a7b4e9fd451d1674e8d4e5
0525stone <0525stone@gmail.com>
2022-05-29 19:00:13 +0900

    aihub 기계독해용 코드 시작

---
+++ tools/data_refine.py
@@ -0,0 +1,28 @@
+#-*- coding: utf-8 -*-
+import re
+import json
+
+def read_file(filename=''):
+    if '.json' in filename:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            data = data['data'] # aihub 기계독해의 경우
+            print(f'type\t{type(data)}\nkeys\t{data.keys()}')
+            
+
+
+
+def main():
+
+    print(f'{"*"*50}    start    {"*"*50}')
+    filename = './data/aihub_mrc/ex1.json'
+    read_file(filename)
+
+    # 최종 파일은 tsv 형식
+    # # id, context, question, answer_text, answer_start
+
+
+
+
+if __name__=="__main__":
+    main()
